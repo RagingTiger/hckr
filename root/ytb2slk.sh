f1f7eb497edcb34a023cddffefdb645ea1fa1a1b
RagingTiger <RagingTiger@yourmom.com>
2019-12-25 11:43:42 -0500

    Source for youtube 2 slack

---
+++ root/ytb2slk.sh
@@ -0,0 +1,10 @@
+# download from youtube, upload to slack
+youtube_2_slack(){
+  local payload="ytbdl.$(date +%m%d%y%H%M%S)"
+  local channel=${2:-'#general'}
+  local message=${3:-"New upload from youtube-dl on $(date)"}
+  youtube-dl --restrict-filename -ciw -o "${payload}"'.%(title)s.%(ext)s' $1 && \
+  slack file upload -fl "${payload}".* -chs ${channel}  -cm "${message}"
+}
+
+youtube_2_slack $1 $2 $3
