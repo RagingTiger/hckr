2e8f368ba73d3f5931d621e0131cd2c3bfd50c80
RagingTiger <RagingTiger@yourmom.com>
2020-04-30 16:46:07 -0400

    Added format option 'best'

--- root/ytb2slk.sh
+++ root/ytb2slk.sh
@@ -3,7 +3,7 @@ youtube_2_slack(){
   local payload="ytbdl.$(date +%m%d%y%H%M%S)"
   local channel=${2:-'#general'}
   local message=${3:-"New upload from youtube-dl on $(date)"}
-  youtube-dl --restrict-filename -ciw -o "${payload}"'.%(title)s.%(ext)s' $1 && \
+  youtube-dl --restrict-filename -f 'best' -ciw -o "${payload}"'.%(title)s.%(ext)s' $1 && \
   slack file upload -fl "${payload}".* -chs ${channel}  -cm "${message}"
 }
 
