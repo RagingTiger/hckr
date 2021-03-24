59e1ab845a725ed88e207c80f008bca57d979d4f
RagingTiger <RagingTiger@yourmom.com>
2021-03-24 17:18:39 -0400

    Updated check for SLACKUP toggle

--- root/insta2slk.sh
+++ root/insta2slk.sh
@@ -1,8 +1,21 @@
 # download from instagram/twitter, upload to slack
 instagram_2_slack(){
-  local message=${2:-"New upload from gallery-dl on $(date)"}
-  local exstr="slack file upload -fl {} -chs '#meeting' -cm '${message}'"
-  gallery-dl --exec "${exstr}" $1
+  # check if pushing to slack
+  if [ ! -z "$SLACKUP" ]; then
+    # create message
+    local message=${2:-"New upload from gallery-dl on $(date)"}
+
+    # create string to be executed
+    local exstr="slack file upload -fl {} -chs '#meeting' -cm '${message}'"
+
+    # start
+    gallery-dl --exec "${exstr}" $1
+
+  else
+    # just run normally
+    gallery-dl $1
+
+  fi
 }
 
-instagram_2_slack $1 $2 
+instagram_2_slack $1 $2
